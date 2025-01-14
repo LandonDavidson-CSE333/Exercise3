a5af4b947d8185c3d5685b6738216ee6c44a7a87
Landon Davidson <accounts@landond.tech>
2025-01-13 18:05:11 -0800

    Add: finished code

--- ex3.c
+++ ex3.c
@@ -1,3 +1,45 @@
-//
-// Created by landond on 1/13/25.
-//
+// Copyright 2025 Landon Davidson
+// Landon Davidson
+// landond@uw.edu
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+// Point3d struct that has the uint16_t properties x, y,
+// and z to represent a point in 3d space
+typedef struct {
+  uint16_t x;
+  uint16_t y;
+  uint16_t z;
+} Point3d;
+
+// Take three uint16_t args and return a Point3d struct
+Point3d* AllocatePoint3d(uint16_t, uint16_t, uint16_t);
+
+// #define testing values for the x, y, and z fields of the struct
+#define X_VAL 10
+#define Y_VAL 20
+#define Z_VAL 30
+// Create a Point3d struct with AllocatePoint3d
+// and test that each field was assigned properly and then free
+int main(int argc, char** argv) {
+  // Store Point3d* returned by AllocatePoint3d
+  Point3d *pt_ptr = AllocatePoint3d(X_VAL, Y_VAL, Z_VAL);
+  // Test that each field of the Point3d struct was assigned properly
+  assert(pt_ptr->x == X_VAL);
+  assert(pt_ptr->y == Y_VAL);
+  assert(pt_ptr->z == Z_VAL);
+  // Free pt_ptr to avoid memory leaks
+  free(pt_ptr);
+}
+
+Point3d* AllocatePoint3d(uint16_t x, uint16_t y, uint16_t z) {
+  // Malloc a block of size Point3d and assigns x, y, z
+  // to Point3d's x, y, z fields and return pointer to Point3d
+  Point3d *pt_ptr = malloc(sizeof(Point3d));
+  pt_ptr->x = x;
+  pt_ptr->y = y;
+  pt_ptr->z = z;
+  return pt_ptr;
+}
